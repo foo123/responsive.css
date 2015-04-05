2b1f479ba9fb37f80c7263d46fca9ef50260b0c3
Nikos M <nikosms0@gmail.com>
2015-04-05 13:20:46 +0300

    upload

---
+++ build.bat
@@ -0,0 +1,17 @@
+@echo off
+
+rem ###################################################
+rem #
+rem #   The buildtools repository is at:
+rem #   https://github.com/foo123/Beeld
+rem #
+rem ###################################################
+
+rem to use the python build tool do (the dependencies file is passed as argument):
+rem python %BUILDTOOLS%\Beeld.py --config ".\beeld.config" --compiler cssmin
+
+rem to use the php build tool do (the dependencies file is passed as argument):
+rem php -f %BUILDTOOLS%\Beeld.php -- --config=".\beeld.config" --compiler=cssmin
+
+rem to use the node build tool do (the dependencies file is passed as argument):
+node %BUILDTOOLS%\Beeld.js --config ".\beeld.config" --compiler cssmin
